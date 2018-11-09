2dc33907912dc52c4bcee24ff3575e080214ce61
Furkan Dinç <36295973+furkandinc@users.noreply.github.com>
2018-11-09 15:47:44 +0300

    Adding dispatcher

--- TheGreatChess/Game.js
+++ TheGreatChess/Game.js
@@ -1,7 +1,25 @@
 var http = require('http');
 var fileSystem = require('fs');
+var httpdispatcher = require('httpdispatcher');
 
-var server = http.createServer(function(req, resp){
+var dispatcher = new httpdispatcher();
+
+function handleRequest(request, response) {
+	try{
+		console.log(request.url);
+		dispatcher.dispatch(request, response);
+	} catch(err) {
+		console.log(err);
+	}
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(process.env.PORT || 5000);
+
+console.log('Listening at: localhost:' + process.env.PORT);
+
+dispatcher.onGet("/", function(req, resp) {
 	fileSystem.readFile('./TheGreatChess/Resources/helloworld.html', function(error, fileContent){
 		if(error){
 			resp.writeHead(500, {'Content-Type': 'text/plain'});
@@ -15,6 +33,7 @@ var server = http.createServer(function(req, resp){
 	});
 });
 
-server.listen(process.env.PORT || 5000);
-
-console.log('Listening at: localhost:' + process.env.PORT);
+dispatcher.onError(function(req, res) {
+	res.writeHead(404);
+	res.end("Sorry we did not get what you are doing!");
+});
---
+++ TheGreatChess/Resources/js/GameScene.js
@@ -0,0 +1,8 @@
+var init = function() {
+
+//--- start example JS ---
+var board = ChessBoard('board');
+//--- end example JS ---
+
+}; // end init()
+$(document).ready(init);
